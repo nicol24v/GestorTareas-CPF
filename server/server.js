7181e7e4ca8507d442cfb6b24adf265c030e82cf
Nicole Vizuete Personal <vizuetenicol@gmail.com>
2026-07-11 00:16:31 -0500

    feat: connect server to MongoDB on startup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const app = require('./src/app');
+const connectDB = require('./src/config/db');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err.message);
+    process.exit(1);
+  });
