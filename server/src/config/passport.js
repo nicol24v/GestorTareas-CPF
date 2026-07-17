a9b99b9118e84d42998ccdb7874927242f629eed
Nicole Vizuete Personal <vizuetenicol@gmail.com>
2026-07-16 22:19:24 -0500

    feat: agregar passport y estrategia de Google OAuth

---
+++ server/src/config/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails?.[0]?.value;
+
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user && email) {
+          user = await User.findOne({ email });
+          if (user) {
+            user.googleId = profile.id;
+            await user.save();
+          }
+        }
+
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email,
+          });
+        }
+
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
+
+module.exports = passport;
